01cc0c7e7505d98514927d333fded89410b1ff35
GerindT <gerindt@gmail.com>
2023-12-13 12:39:40 +0100

    fixes on scrapping, getting all product details

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,102 @@ function deriveReviewsURL(productURL) {
   return reviewsURL;
 }
 
+async function scrapeProductDetails(url) {
+  const data = { description: "" };
+
+  let customArgs = [
+    "--disable-setuid-sandbox",
+    "--no-sandbox",
+    "--single-process",
+    "--no-zygote",
+  ];
+
+  const browser = await launch({
+    headless: "New",
+    args: process.env.NODE_ENV === "production" ? customArgs : [],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
+
+  const page = await browser.newPage();
+
+  await page.goto(`${url}`, {
+    waitUntil: "domcontentloaded",
+  });
+
+  // Scrape the price
+  const price = await page.$eval("span.a-offscreen", (element) =>
+    element.textContent.trim()
+  );
+  console.log(`Price: ${price}`);
+  data["price"] = price;
+
+  // Scrape the product title
+  const productTitle = await page.$eval("span#productTitle", (element) =>
+    element.textContent.trim()
+  );
+  console.log(`Product Title: ${productTitle}`);
+  data["productTitle"] = productTitle;
+  // Scrape the product details
+  const productDetails = await page.$$eval(
+    "div.product-facts-detail",
+    (elements) =>
+      elements.map((element) => {
+        const key = element
+          .querySelector(
+            "div.a-fixed-left-grid-col.a-col-left span.a-color-base"
+          )
+          .textContent.trim();
+        const value = element
+          .querySelector(
+            "div.a-fixed-left-grid-col.a-col-right span.a-color-base"
+          )
+          .textContent.trim();
+        return { [key]: value };
+      })
+  );
+  console.log(`Product Details: ${JSON.stringify(productDetails)}`);
+  data["productDetails"] = productDetails;
+
+  // Scrape the about item information
+  const descriptionItem = await page.$$eval(
+    "ul.a-unordered-list.a-vertical.a-spacing-small li span.a-list-item",
+    (elements) => elements.map((item) => item.textContent.trim())
+  );
+  const descriptionItemText = descriptionItem.join(" ");
+  console.log(`About Item: ${descriptionItemText}`);
+  data["descriptionItem"] = descriptionItemText;
+
+  // Scrape the product details
+  const generalProductDetails = await page.$$eval(
+    "table.a-normal.a-spacing-micro tr",
+    (elements) =>
+      elements.map((element) => {
+        const key = element
+          .querySelector("td.a-span3 span.a-size-base.a-text-bold")
+          .textContent.trim();
+        const value = element
+          .querySelector("td.a-span9 span.a-size-base.po-break-word")
+          .textContent.trim();
+        return { [key]: value };
+      })
+  );
+  console.log(`Product Details: ${JSON.stringify(generalProductDetails)}`);
+  data["generalProductDetails"] = generalProductDetails;
+
+  // Scrape the description
+  const generalDescriptionItem = await page.$$eval(
+    "ul.a-unordered-list.a-vertical.a-spacing-mini li.a-spacing-mini span.a-list-item",
+    (elements) => elements.map((item) => item.textContent.trim())
+  );
+  const description = generalDescriptionItem.join(" ");
+  data["generalDescriptionItem"] = description;
+
+  return data;
+}
+
 async function scrapeReviewsByRating(url, filterRating) {
   let customArgs = [
     "--disable-setuid-sandbox",
@@ -40,15 +136,10 @@ async function scrapeReviewsByRating(url, filterRating) {
 
   const page = await browser.newPage();
 
-  await page.goto(`${url}&filterByStar=${filterRating}`, {
+  const reviewsUrl = deriveReviewsURL(url);
+  await page.goto(`${reviewsUrl}&filterByStar=${filterRating}`, {
     waitUntil: "domcontentloaded",
   });
-
-  const title = await page.$eval('div[data-hook="review"]', (element) =>
-    element.textContent.trim()
-  );
-  console.log(`Reviews for ${filterRating} stars - ${title}`);
-
   let pageNum = 1;
   let flRating = filterRating;
   const data = {};
@@ -91,15 +182,6 @@ async function scrapeReviewsByRating(url, filterRating) {
         'span[data-hook="review-date"]',
         (element) => element.textContent.trim()
       );
-      let itemDetails = "";
-      try {
-        itemDetails = await review.$eval(
-          'a[data-hook="format-strip"]',
-          (element) => element.textContent.trim()
-        );
-      } catch (error) {
-        console.error("Error fetching item details:", error.message);
-      }
 
       console.log(`Author: ${author}`);
       console.log(`Review: ${reviewText}`);
@@ -140,18 +222,25 @@ export async function runCrawler(urls) {
   const dataset = [];
   const starRatings = [
     "five_star",
-    "four_star",
-    "three_star",
-    "two_star",
-    "one_star",
+    // "four_star",
+    // "three_star",
+    // "two_star",
+    // "one_star",
   ]; // Change this array based on your needs
 
   for (const [index, url] of urls.entries()) {
     console.log(`Scraping reviews for ${index}`);
-    dataset.push({ url, totalreviews: [] });
+    if (!dataset[index]) {
+      dataset[index] = { url, totalreviews: [] };
+    }
+    const baseData = await scrapeProductDetails(urls);
+
+    console.log(baseData);
+
+    dataset[index] = { ...dataset[index], ...baseData };
     for (const rating of starRatings) {
-      const data = await scrapeReviewsByRating(deriveReviewsURL(url), rating);
-      for (const review of data[rating]) {
+      const reviewData = await scrapeReviewsByRating(url, rating);
+      for (const review of reviewData[rating]) {
         const timeString = review.time;
         const dateRegex = /on (.*)/;
         const match = timeString.match(dateRegex);
@@ -163,12 +252,12 @@ export async function runCrawler(urls) {
           review.date = null;
         }
       }
-      dataset[index].totalreviews.push(...data[rating]);
+      dataset[index].totalreviews.push(...reviewData[rating]);
 
       if (!dataset[index].hasOwnProperty(rating)) {
         dataset[rating] = []; // Initialize the array if it doesn't exist
       }
-      dataset[index][rating] = data[rating];
+      dataset[index][rating] = reviewData[rating];
     }
   }
 
