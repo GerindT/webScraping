fc5eb98e17136aded41587886e03b3069f8764e4
GerindT <gerindt@gmail.com>
2023-12-13 14:04:26 +0100

    test

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 import { launch } from "puppeteer";
 import puppeteer from "puppeteer";
 import dotenv from "dotenv";
-import { Cluster } from "puppeteer-cluster";
 dotenv.config();
 
 function deriveReviewsURL(productURL) {
@@ -28,7 +27,7 @@ async function scrapeProductDetails(url) {
   let customArgs = [
     "--disable-setuid-sandbox",
     "--no-sandbox",
-    "--single-process",
+    // "--single-process",
     "--no-zygote",
   ];
 
@@ -227,25 +226,25 @@ export async function runCrawler(urls) {
     "three_star",
     "two_star",
     "one_star",
-  ];
+  ]; // Change this array based on your needs
 
-  // Create a puppeteer cluster
-  const cluster = await Cluster.launch({
-    concurrency: Cluster.CONCURRENCY_CONTEXT,
-    maxConcurrency: 4, // Adjust this value based on your needs
-  });
+  for (const [index, url] of urls.entries()) {
+    console.log(`Scraping reviews for ${index}`);
+    if (!dataset[index]) {
+      dataset[index] = { url, totalreviews: [] };
+    }
+    const baseData = await scrapeProductDetails(urls);
 
-  // Define a task (this is where your scraping code goes)
-  await cluster.task(async ({ page, data: url }) => {
-    const baseData = await scrapeProductDetails(url);
-    dataset.push({ url, ...baseData, totalreviews: [] });
+    console.log(baseData);
 
+    dataset[index] = { ...dataset[index], ...baseData };
     for (const rating of starRatings) {
       const reviewData = await scrapeReviewsByRating(url, rating);
       for (const review of reviewData[rating]) {
         const timeString = review.time;
         const dateRegex = /on (.*)/;
         const match = timeString.match(dateRegex);
+
         if (match) {
           const dateString = match[1];
           review.date = new Date(dateString);
@@ -253,22 +252,14 @@ export async function runCrawler(urls) {
           review.date = null;
         }
       }
-      dataset[dataset.length - 1].totalreviews.push(...reviewData[rating]);
-      if (!dataset[dataset.length - 1].hasOwnProperty(rating)) {
-        dataset[dataset.length - 1][rating] = []; // Initialize the array if it doesn't exist
+      dataset[index].totalreviews.push(...reviewData[rating]);
+
+      if (!dataset[index].hasOwnProperty(rating)) {
+        dataset[rating] = []; // Initialize the array if it doesn't exist
       }
-      dataset[dataset.length - 1][rating] = reviewData[rating];
+      dataset[index][rating] = reviewData[rating];
     }
-  });
-
-  // Queue URLs
-  for (const url of urls) {
-    cluster.queue(url);
   }
-
-  // Wait for the cluster to idle and close it
-  await cluster.idle();
-  await cluster.close();
 
   console.log(dataset);
   for (const data of dataset) {
